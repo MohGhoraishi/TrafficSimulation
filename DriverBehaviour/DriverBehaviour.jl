8c57ff9439981ac01012431ebec51665f5040360
Mohammad Ghoraishi <mohammadg2002@outlook.com>
2023-09-21 20:50:33 +0330

    started stage 1 - simple model

---
+++ DriverBehaviour/DriverBehaviour.jl
@@ -0,0 +1,10 @@
+using LinearAlgebra
+
+#initial parameters - Simple Driver Behavior parameters
+ARTdriver = 0.75
+TCLdriver = 0.5
+TACLdriver = 0.5
+
+#Road Property parameters
+NLroad = 3
+SLroad = 80
