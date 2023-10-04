df48b06982e60b70ffb8bc82d7ce5a19a7a1ff10
Mohammad Ghoraishi <mohammadg2002@outlook.com>
2023-10-04 12:25:17 +0330

    Added resolution parameter to initializeRoad function

--- DriverBehaviour/DriverBehaviour.jl
+++ DriverBehaviour/DriverBehaviour.jl
@@ -11,19 +11,22 @@ Initializes a road with a random distribution of cars with a selected total dens
 * `numberLanesRoad` = the number of lanes in our road.
 * `startingDensity` = the density of cars we want on our road.
 * `averageCarLengthDriver` = the average langth of a car.
+* `resolution` = Splits up the available space into smaller chunks
 
 # Notes
 * The function is randomized and does not fill the road with the exact density that is specified
 
 """
 
-function initializeRoad(numberLanesRoad::Integer, startingDensity::Float64, averageCarLengthDriver::Float64, lengthRoad) #initializes traffic by a starting density ranging from 0 to 1
+function initializeRoad(numberLanesRoad::Integer, startingDensity::Float64, averageCarLengthDriver::Float64, lengthRoad, resolution::Integer) #initializes traffic by a starting density ranging from 0 to 1
     availableSpacesPerLane = floor(Int, lengthRoad / averageCarLengthDriver)
-    posMatrix = zeros(availableSpacesPerLane, numberLanesRoad)
+    posMatrix = zeros(availableSpacesPerLane * resolution, numberLanesRoad)
     for i in 1:numberLanesRoad
-        for j in 1:availableSpacesPerLane
+        for j in 1:availableSpacesPerLane - 1
             if rand() < startingDensity
-                posMatrix[j, i] = 1
+                for k in 0:resolution-1
+                    posMatrix[j * resolution + k, i] = 1
+                end
             end
         end
     end
